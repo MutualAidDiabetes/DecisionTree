ed7a3a193b0a43b47ef724868c577af5425b717b
seiyria <kyle@seiyria.com>
2020-12-20 18:36:23 -0600

    refactor code to be es6

--- index.js
+++ index.js
@@ -1,54 +1,62 @@
 
+class DecisionTree {
 
-var DecisionTree = function(data) {
-  
-  this.initial = data.initial;
-  this.choices = data.choices;
-  
-  /* Return an array of choice objects for the root of the tree */
-  this.getInitial = function() {
-    
-    if (!this.initial) throw 'DecisionTree: no initial choice(s) specified';
-    return this.getChoices(this.initial);
-  };
-  
-  /* Get full choice data by specific id */
-  this.getChoice = function(id) {
+  constructor(data) {
+    this.initial = data.initial;
+    this.choices = data.choices;
 
-    if (!(id in this.choices)) return false;
-    if (!('id' in this.choices[id])) this.choices[id].id = id;
-    return this.choices[id];
+    this.init();
+  }
+
+  init() {
+    const idList = [];
     
-  };
-  
-  /* As above, but passing an array of choice IDs */
-  this.getChoices = function(idList) {
-    if(!idList) return [];
-    var list = [];
-    for(var i = 0, ln = idList.length; i < ln; i++) {
-      var childChoice = this.getChoice(idList[i]);
-      list.push(childChoice);
+    for(var k in this.choices) {
+      if(idList.indexOf(k) !== -1) throw new Error(`DecisionTree: duplicate ID "${k}" in choice set`);
+      
+      const choice = this.getChoice(k);
+      choice.id = k;
+      
+      const children = this.getChildren(k);
+
+      children.forEach(child => {
+        if(child.parent) throw new Error(`DecisionTree: tried to assign parent "${k}" to child "${child}" which already has parent "${child.parent}"`);
+
+        child.parent = k;
+      });
+      
     }
-    return list;
-    
-  };
-  
-  /* Get an array of choice data for a parent id */
-  this.getChildren = function(parentId) {
-    
-    if (!(parentId in this.choices)) return false;
-    if (!('children' in this.choices[parentId])) return false;
-    
-    var childIds = this.choices[parentId].children;
-    return this.getChoices(childIds);
-    
-  };
-  
-  /* Get an array of choice data for the parent(s) of an id */
-  this.getParents = function(id) {
-    
-    var parents = [];
-    var node = this.getChoice(id);
+  }
+
+  getInitial() {
+    if (!this.initial) throw new Error('DecisionTree: no initial choice(s) specified');
+    return this.getChoices(this.initial);
+  }
+
+  getChoice(id) {
+    const choice = this.choices[id];
+    if(!choice) return;
+    if(!choice.id) choice.id = id;
+
+    return choice;
+  }
+
+  getChoices(idList) {
+    if(!idList) return [];
+
+    return idList.map(x => this.getChoice(x));
+  }
+
+  getChildren(parentId) {
+    const choice = this.choices[parentId];
+    if(!choice || !choice.children) return;
+
+    return this.getChoices(choice.children);
+  }
+
+  getParents(id) {
+    const parents = [];
+    let node = this.getChoice(id);
     
     while(node.parent) {
       node = this.getChoice(node.parent);
@@ -56,70 +64,32 @@ var DecisionTree = function(data) {
     }
     
     return parents;
-    
-  };
-  
-  /* Get just an array of ids for the parents of a specific id */
-  this.getParentIds = function(id) {
-    var parents = this.getParents(id);
-    var parentIds = [];
-    for(var i = 0, ln = parents.length; i < ln; i++) {
-      parentIds.push(parents[i].id);
-    }
-    return parentIds;
-  };
-  
-  /* Get the 'name' prop for the parent of an id */
-  this.getParentName = function(id) {
-    var parent = this.getParents(id).pop();
-    if(!parent) {
-      return false;
-    } else {
-      return parent.name;
-    }
-  };
-  
-  /* Get the 'name' prop for the parent of an id */
-  this.getParentSubtitle = function(id) {
-    var parent = this.getParents(id).pop();
-    if(!parent) {
-      return false;
-    } else {
-      return parent.subtitle;
-    }
-  };
-  
-  /* Init - insert ids into choice objects, check dups, associate parents, etc */
-  this.init = function() {
-    
-    var idList = [];
-    for(var k in this.choices) {
-      if(idList.indexOf(k) !== -1) throw 'DecisionTree: duplicate ID "' + k + '" in choice set';
-      
-      var choice = this.getChoice(k);
-      choice.id = k;
-      
-      var children = this.getChildren(k);
-      for(var i = 0; i < children.length; i++) {
-        
-        var child = children[i];
-        if(child.parent) throw 'DecisionTree: tried to assign parent "' + k + '" to child "' + choice.children[i] + '" which already has parent "' + child.parent + '"';
-        child.parent = k;
-       
-      }
-      
-    }
-        
-  };
-  
-  this.init();
-  
-};
+  }
 
+  getParentIds(id) {
+    const parents = this.getParents(id);
 
-/*** TEST DATA ***/
+    return parents.map(p => p.id);
+  };
 
-var data = {
+  getParentName(id) {
+    const parent = this.getParents(id).pop();
+    if(!parent) return false;
+
+    return parent.name;
+  }
+
+  getParentSubtitle(id) {
+    const parent = this.getParents(id).pop();
+    if(!parent) return false;
+
+    return parent.subtitle;
+  }
+
+}
+
+/** Data to pass into tree */
+const data = {
 
   initial: [
     'insulin'
@@ -341,35 +311,27 @@ var data = {
   }
 };
 
-/** TEST CODE **/
+/** Init tree, events */
+$(() => {
+  const tree = new DecisionTree(data);
+  const $list = $('#choices');
+  const $title = $('#title');
+  const $subtitle = $('#subtitle');
+  const $help = $('#help');
+  const $back = $('#back');
 
-$(function() {
+  $back.hide();
   
-  var tree = new DecisionTree(data);
-  var $list = $('#choices');
-  var $title = $('#title');
-  var $subtitle = $('#subtitle');
-  var $help = $('#help');
+  let currentId = null;
   
-  var current_id = null;
-  
-  var renderList = function(items) {
-
+  const renderList = (items) => {
     if(!items || !items[0]) return;
     
-    var title = tree.getParentName(items[0].id);
-    if(title) {
-      $title.text(title);
-    } else {
-      $title.text('What is your current situation?');
-    }
+    const title = tree.getParentName(items[0].id) || 'What is your current situation?';
+    $title.text(title);
     
-    var subtitle = tree.getParentSubtitle(items[0].id);
-    if(subtitle) {
-      $subtitle.text(subtitle);
-    } else {
-      $subtitle.text('');
-    }
+    const subtitle = tree.getParentSubtitle(items[0].id) || '';
+    $subtitle.text(subtitle);
     
     $list.empty();
 
@@ -382,55 +344,56 @@ $(function() {
     });
   };
 
-  var renderHelp = function(help) {
-    if(help) {
-      $help.text(help);
-    } else {
-      $help.text('');
-    }
-  }
-  
-  var _doInitial = function() {
-      var initData = tree.getInitial();
-      current_id = null;
-      renderList(initData);
+  const renderHelp = (help = '') => {
+    $help.text(help);
   };
   
-  $(document).on('click', '#choices a', function(e) {
+  const renderInitial = () => {
+    const initData = tree.getInitial();
+    currentId = null;
+    renderList(initData);
+  };
+  
+  $(document).on('click', '#choices a', (e) => {
     e.preventDefault();
-    var choiceId = $(this).data('choice');
-    var help = $(this).data('help');
     
-    var kids = tree.getChildren(choiceId);
+    const choiceId = $(e.target).data('choice');
+    const help = $(e.target).data('help');
+    const kids = tree.getChildren(choiceId);
+
+    $back.show();
 
     if(kids && kids.length > 0) {
-      current_id = choiceId;
+      currentId = choiceId;
       renderList(kids);
       renderHelp('');
+
     } else if(help) {
       renderHelp(help);
     }
   });
   
-  $('#back').on('click', function(e) {
+  $back.on('click', (e) => {
     e.preventDefault();
     renderHelp('');
 
-    if(!current_id) return false;
+    if(!currentId) return false;
     
-    var parents = tree.getParents(current_id);
+    const parents = tree.getParents(currentId);
     
     if(parents.length > 0) {
-      var prev_node = parents.pop();
-      current_id = prev_node.id;
-      renderList(tree.getChildren(prev_node.id));
+      const prevNode = parents.pop();
+      currentId = prevNode.id;
+      renderList(tree.getChildren(prevNode.id));
+
     } else {
-      _doInitial();
+      renderInitial();
+      $back.hide();
     }
     
   });
 
-  _doInitial();
+  renderInitial();
 
   
 });
